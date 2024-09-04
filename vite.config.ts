cd509e76b0634744bcff77a3cc0f1b84d2dac353
juan <jjhuertasbotache@gmail.com>
2024-09-04 15:59:19 -0500

    � planning new functions

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Tasky/', // Asegúrate de reemplazar 'nombre-del-repo' por el nombre de tu repositorio
 })
+
